506ad0c5c24b0694d5db6bf76f71f2febe4a3e48
Emil Fresk <emil.fresk@gmail.com>
2022-11-03 19:47:41 +0100

    Now with multi support

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,16 @@
 
 mod cortex_m_interrupt {
 
-    pub trait InterruptRegistration<T> {
-        const VECTOR: T; // Holds vector name for compiletime errors
+    pub trait InterruptRegistration<Vector> {
+        const VECTOR: Vector; // Holds vector name for compiletime errors
 
         fn on_interrupt();
     }
 
-    /// This is implemented by codegen, `T` is needed as this crate does not
-    /// know the type of the PAC that will be used.
-    pub trait InterruptHandle {
-        fn activate(self); // Enable the registered interrupt
+    pub trait InterruptToken<Periperhal> {
+        // fn activate(self); // Enable the registered interrupt
 
-        fn override_priority(&mut self, priority: u8); // New priority
+        // fn override_priority(&mut self, priority: u8); // New priority
     }
 }
 
@@ -29,16 +27,22 @@ mod cortex_m_interrupt {
 pub mod pac {
     pub struct SPI0;
 
+    pub struct UART0;
+
+    pub struct UART1;
+
     pub enum Interrupt {
         Int1,
         Int2,
         Int3,
         Spi0,
+        Uart0_1,
+        Uart3,
     }
 }
 
 pub mod hal {
-    use crate::cortex_m_interrupt::{InterruptHandle, InterruptRegistration};
+    use crate::cortex_m_interrupt::{InterruptRegistration, InterruptToken};
 
     pub use crate::pac;
 
@@ -49,14 +53,8 @@ pub mod hal {
     impl Spi {
         pub fn new<Handle>(spi: pac::SPI0, interrupt_handle: Handle) -> Self
         where
-            Handle: InterruptHandle,
+            Handle: InterruptToken<Spi>,
         {
-            // const_assert!(Handle::VECTOR == pac::Interrupt::SpiIsr);
-
-            // setup the peripheral ...
-
-            interrupt_handle.activate();
-
             Spi {}
         }
     }
@@ -70,13 +68,59 @@ pub mod hal {
             // Doing stuff ...
         }
     }
+
+    //
+    // ---
+    //
+
+    pub struct Uart0 {}
+
+    impl Uart0 {
+        pub fn new<Handle>(uart: pac::UART0, interrupt_handle: Handle) -> Self
+        where
+            Handle: InterruptToken<Uart0>,
+        {
+            Uart0 {}
+        }
+    }
+
+    impl InterruptRegistration<pac::Interrupt> for Uart0 {
+        const VECTOR: pac::Interrupt = pac::Interrupt::Uart0_1;
+
+        // It might have a dependency that you can't call `handle.activate()`
+        // until peripheral setup is complete.
+        fn on_interrupt() {
+            // Doing stuff ...
+        }
+    }
+
+    pub struct Uart1 {}
+
+    impl Uart1 {
+        pub fn new<Handle>(uart: pac::UART1, interrupt_handle: Handle) -> Self
+        where
+            Handle: InterruptToken<Uart1>,
+        {
+            Uart1 {}
+        }
+    }
+
+    impl InterruptRegistration<pac::Interrupt> for Uart1 {
+        const VECTOR: pac::Interrupt = pac::Interrupt::Uart0_1;
+
+        // It might have a dependency that you can't call `handle.activate()`
+        // until peripheral setup is complete.
+        fn on_interrupt() {
+            // Doing stuff ...
+        }
+    }
 }
 
 //
 // User code
 //
 pub fn test() {
-    use crate::cortex_m_interrupt::{InterruptHandle, InterruptRegistration};
+    use crate::cortex_m_interrupt::{InterruptRegistration, InterruptToken};
 
     // let handle = register_interrupt!(
     // 	hal::pac::Interrupt::Spi0, // Full path to interrupt to register to
@@ -102,20 +146,52 @@ pub fn test() {
 
         struct Handle(u8);
 
-        impl InterruptHandle for Handle {
-            fn activate(self) {
-                // TODO: Poke the NVIC
-                // - enable interrupts
-                // - setup prio
-
-                atomic_polyfill::compiler_fence(atomic_polyfill::Ordering::Release);
-            }
-
-            fn override_priority(&mut self, priority: u8) {
-                self.0 = priority;
-            }
-        }
+        impl InterruptToken<hal::Spi> for Handle {}
 
         Handle(3) // prio goes here
     };
+
+    let spi = hal::Spi::new(pac::SPI0 {}, handle);
+
+    // let handle = register_interrupt!(
+    // 	hal::pac::Interrupt::Uart0_1, // Full path to interrupt to register to
+    // 	hal::Uart0, hal::Uart1, // Struct implementing `InterruptRegistration`
+    // 	2, // Optional priority
+    // );
+
+    // => codegen
+
+    let handle2 = {
+        const _: () = {
+            match <hal::Uart0 as InterruptRegistration<pac::Interrupt>>::VECTOR {
+                pac::Interrupt::Uart0_1 => {}
+                _ => panic!("Wrong vector"),
+            }
+        };
+
+        const _: () = {
+            match <hal::Uart1 as InterruptRegistration<pac::Interrupt>>::VECTOR {
+                pac::Interrupt::Uart0_1 => {}
+                _ => panic!("Wrong vector"),
+            }
+        };
+
+        #[export_name = "Uart0_1"]
+        #[allow(non_snake_case)]
+        pub unsafe extern "C" fn interrupt() {
+            <hal::Uart0 as InterruptRegistration<pac::Interrupt>>::on_interrupt();
+            <hal::Uart1 as InterruptRegistration<pac::Interrupt>>::on_interrupt();
+        }
+
+        #[derive(Copy, Clone)]
+        struct Handle;
+
+        impl InterruptToken<hal::Uart0> for Handle {}
+        impl InterruptToken<hal::Uart1> for Handle {}
+
+        Handle {}
+    };
+
+    let uart0 = hal::Uart0::new(pac::UART0 {}, handle2);
+    let uart1 = hal::Uart1::new(pac::UART1 {}, handle2);
 }
